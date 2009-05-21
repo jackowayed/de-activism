c9c9f001dda7f48f9528ca83e5266c5e7da977b8
Daniel Jackoway <danjdel@gmail.com>
2009-05-21 16:02:37 -0400

    parenthesize return haml statements because ruby compalins

--- dereps.rb
+++ dereps.rb
@@ -33,21 +33,21 @@ post '/script' do
     @errors = "If you don't provide a 9-digit zip code, you must provide your address, city, and state" unless @person.address && @person.city && @person.state
   end
 
-  return haml :index if @errors
+  return haml(:index) if @errors
 
 
   @person.zip9 = param_load(params[:zip9]) || zip_9(@person)
 
   unless @person.zip9
     @errors = "We had an error getting your 9-digit zip code. Please use link to manually find it."
-    return haml :index
+    return haml(:index)
   end
 
   @person.set_reps
 
   unless @person.sd
     @errors = "We had some issue finding your state senator. Sorry about that! Trying once more now and then again in 5 or 10 minutes might be all it takes. Otherwise, please send us feedback"
-    return haml :index
+    return haml(:index)
   end
   
   
