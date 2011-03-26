66c29cce064c7c05740dea139da6d1c495e1218b
Daniel Jackoway <danjdel@gmail.com>
2011-03-26 16:56:00 -0700

    got it running with new sinatra

--- dereps.rb
+++ dereps.rb
@@ -3,6 +3,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'state-reps'
+require 'haml'
 
 include StateReps
 
@@ -23,7 +24,7 @@ get '/' do
   haml :index
 end
 
-post '/script' do
+post '/call' do
   @person = Person.new
   @person.address = param_load params[:address]
   @person.city = param_load params[:city]
@@ -53,161 +54,5 @@ post '/script' do
   end
   
   
-  haml :script
+  haml :call
 end
-
-
-
-use_in_file_templates!
-
-__END__
-
-
-@@ index
-
-%h1= BILL
-
-- if @errors
-  #errors
-    = @errors
-
-%p
-  %span.phonenum UPDATE:
-  A new bill, Senate Bill 121, that is virtually identical, was introduced to a different committee and has passed. Governor Markell is expected to sign it on July 2nd.
-  %a{:href => 'http://www.delawareliberal.net/2009/06/27/sb-121-signing-today/'} More Info
-
-%p
-  Delaware House Bill 5 proposes to ban discrimination based on sexual orientation. It passed the state House of Representatives convincingly with a 26-14 margin. Currently, it is stuck in the Senate Executive Committee. 
-%p
-  Since the bill will probably not leave committee through regular means, state Senators need to sign a petition to bring it to the Senate floor.
-%p
-  This site will quickly and securely allow you to determine your state Senator and his or her phone number and give you a sample script to use when encouraging him or her to sign the petition. 
-
-%p
-  First, we must determine your state representatives. To do so, please enter your address below. We will not record your address. It will only be used to determine your state Senator.
-
-%form{:action => '/script', :method => :post}
-  %p
-    %label Address
-    %input{:type => 'textfield', :name => "address", :size => 50}
-  %p
-    %label City
-    %input{:type => 'textfield', :name => "city", :size => 25}
-    %label State
-    -# thanks http://snippets.dzone.com/posts/show/375
-    <select name="state"> 
-    <option value="AL">Alabama</option> 
-    <option value="AK">Alaska</option> 
-    <option value="AZ">Arizona</option> 
-    <option value="AR">Arkansas</option> 
-    <option value="CA">California</option> 
-    <option value="CO">Colorado</option> 
-    <option value="CT">Connecticut</option> 
-    <option value="DE" selected="selected">Delaware</option> 
-    <option value="DC">District Of Columbia</option> 
-    <option value="FL">Florida</option> 
-    <option value="GA">Georgia</option> 
-    <option value="HI">Hawaii</option> 
-    <option value="ID">Idaho</option> 
-    <option value="IL">Illinois</option> 
-    <option value="IN">Indiana</option> 
-    <option value="IA">Iowa</option> 
-    <option value="KS">Kansas</option> 
-    <option value="KY">Kentucky</option> 
-    <option value="LA">Louisiana</option> 
-    <option value="ME">Maine</option> 
-    <option value="MD">Maryland</option> 
-    <option value="MA">Massachusetts</option> 
-    <option value="MI">Michigan</option> 
-    <option value="MN">Minnesota</option> 
-    <option value="MS">Mississippi</option> 
-    <option value="MO">Missouri</option> 
-    <option value="MT">Montana</option> 
-    <option value="NE">Nebraska</option> 
-    <option value="NV">Nevada</option> 
-    <option value="NH">New Hampshire</option> 
-    <option value="NJ">New Jersey</option> 
-    <option value="NM">New Mexico</option> 
-    <option value="NY">New York</option> 
-    <option value="NC">North Carolina</option> 
-    <option value="ND">North Dakota</option> 
-    <option value="OH">Ohio</option> 
-    <option value="OK">Oklahoma</option> 
-    <option value="OR">Oregon</option> 
-    <option value="PA">Pennsylvania</option> 
-    <option value="RI">Rhode Island</option> 
-    <option value="SC">South Carolina</option> 
-    <option value="SD">South Dakota</option> 
-    <option value="TN">Tennessee</option> 
-    <option value="TX">Texas</option> 
-    <option value="UT">Utah</option> 
-    <option value="VT">Vermont</option> 
-    <option value="VA">Virginia</option> 
-    <option value="WA">Washington</option> 
-    <option value="WV">West Virginia</option> 
-    <option value="WI">Wisconsin</option> 
-    <option value="WY">Wyoming</option>
-    </select>
-  .phonenum OR
-  %hr{:width => '500px', :style => "margin-left: 0"}
-  %p
-    If you are uncomfortable providing your address, simply enter your 9-digit zip code below. Go
-    %a{:href => "http://zip4.usps.com/zip4/"} here
-    to determine your 9-digit zip code if you do not know it.
-  %p
-    %label 9-digit Zip Code
-    %input{:type => "textfield", :name => "zip9", :length => 10, :size => 10}
-  %input{:type => "submit", :value => "Continue"}
-
-@@ script
-
-%h1 Call Your Senator
-
-%p
-  A new bill with the same goals, Senate Bill 121, has been introduced. Please urge your Senator to do whatever they can to ensure that one of these passes. 
-
-%p
-  Now, please call your state senator, Senator
-  = precede @person.senator_name do
-    , to ask him/her to sign the petition to get
-  = BILL
-  out of committee.
-
-%p
-  Below is your senator's phone number, followed by a sample script to use when talking to him or her.
-
-%p
-  Call
-  %span.phonenum= @person.senator_phone
-
-%h2 Script
-
-%p
-  Dear Senator 
-  =precede @person.senator_name do
-    \:
-
-%p
-  My name is 
-
-  = succeed '. I live at' do
-    %span.bold&== <your name>
-  = succeed ", and I am calling today to ask for your support for #{BILL}." do
-    - if @person.address
-      = @person.full_address
-    - else
-      %span.bold&== <your address>
-
-
-%p
-  = SHORT_BILL
-  are bills that would prohibit discrimination on the basis of sexual orientation in areas such as employment and housing. These bills are important to me because I feel that it is important to end this kind of unfair discrimination.
-
-%p
-  HB 5 passed by a large margin in the House of Representatives, but is stuck in the Senate Executive Committee. Senate Bill 121 has been introduced to a different committee, the Senate Insurance Committee, with the same intent as HB 5. 
-
-%p
-  Please do whatever you can to get either of these bills onto the floor. We have been patiently waiting for 10 years to get this legislation to the senate floor. 
-
-%p
-  Can I count on your support in getting HB 5 and SB 121 out of committee?
