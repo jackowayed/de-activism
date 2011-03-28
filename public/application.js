a478ce507e2dab69af76da6b345395e3fcf391a3
Daniel Jackoway <danjdel@gmail.com>
2011-03-27 20:14:09 -0700

    tweak tweak

--- public/application.js
+++ public/application.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
 	$('.switch').click(function() {
     mpmetrics.track('Switch callee');
-		if ($('.switch')[0].innerHTML == "Switch to Calling Your Representative") {
+		if ($('.switch')[0].innerHTML == "Switch to Contacting Your Representative") {
 			$('.rep').show();
       $('.sen').hide();
-      $('.switch').text("Switch to Calling Your Senator");
+      $('.switch').text("Switch to Contacting Your Senator");
 		}
 		else {
 			$('.rep').hide();
       $('.sen').show();
-      $('.switch').text("Switch to Calling Your Representative");
+      $('.switch').text("Switch to Contacting Your Representative");
 		}
   })
 })
